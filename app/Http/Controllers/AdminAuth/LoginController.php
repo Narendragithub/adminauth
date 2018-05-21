9f7d68dc553283fdc993549bd92f022518806c1e
Narendra <Narendra@indore.local>
2018-05-21 11:16:09 +0530

    logincontroller

--- app/Http/Controllers/AdminAuth/LoginController.php
+++ app/Http/Controllers/AdminAuth/LoginController.php
@@ -38,9 +38,10 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        
         $this->middleware('admin.guest', ['except' => 'logout']);
     }
-    
+
 
     /**
      * Show the application's login form.
